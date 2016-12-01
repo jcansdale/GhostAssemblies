bc223352a7893b054e76c351db79ad286e3a5f99
Jamie Cansdale <jcansdale@gmail.com>
2016-12-01 16:35:24 +0000

    Add GhostAssemblies project and tests `GhostAssemblyLoader` enables the loading, resolving and refreshing of a group of "ghost" assemblies. This lets an extension be developed live inside a host process without the constant need to relaunch the host. It was designed with Visual Studio package development in mind.

---
+++ src/GhostAssemblies/GhostAssemblyLoader.cs
@@ -0,0 +1,226 @@
+﻿namespace GhostAssemblies
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Reflection;
+
+    public class GhostAssemblyLoader : IDisposable
+    {
+        string installDir;
+        string defaultAssemblyName;
+        IDictionary<string, GhostAssembly> ghostAssemblies;
+
+        public GhostAssemblyLoader(string ghostAssemblyPaths, string defaultAssemblyName = null, string installDir = null)
+        {
+            this.defaultAssemblyName = defaultAssemblyName;
+            this.installDir = installDir ?? getDirectory(System.Reflection.Assembly.GetCallingAssembly());
+
+            var ghostAssemblyFiles = getAssemblyFiles(ghostAssemblyPaths);
+            ghostAssemblies = createGhostAssemblies(ghostAssemblyFiles);
+
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        static string[] getAssemblyFiles(string paths)
+        {
+            if(paths == null)
+            {
+                return new string[0];
+            }
+
+            return paths.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public void Dispose()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve -= CurrentDomain_AssemblyResolve;
+        }
+
+        System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var requestingAssembly = args.RequestingAssembly;
+            if (isGhost(requestingAssembly))
+            {
+                return ResolveAssembly(args.Name);
+            }
+
+            return null;
+        }
+
+        public System.Reflection.Assembly ResolveAssembly(string name = null)
+        {
+            if(name == null)
+            {
+                if(defaultAssemblyName == null)
+                {
+                    var message = GhostAssemblyException.NoDefaultGhostMessage;
+                    throw new GhostAssemblyException(message);
+                }
+
+                name = defaultAssemblyName;
+            }
+
+            var assemblyName = new AssemblyName(name);
+
+            // Resolve known "ghost" assembly.
+            var ghostAssembly = findGhostAssembly(assemblyName.Name);
+            if (ghostAssembly != null)
+            {
+                return ghostAssembly.GetAssembly();
+            }
+
+            // Resolve unknown assembly.
+            var assemblyFile = findAssemblyFile(installDir, assemblyName.Name);
+            if (assemblyFile != null)
+            {
+                return Assembly.LoadFrom(assemblyFile);
+            }
+
+            return null;
+        }
+
+        static string getDirectory(System.Reflection.Assembly assembly)
+        {
+            var localPath = new Uri(assembly.EscapedCodeBase).LocalPath;
+            return Path.GetDirectoryName(localPath);
+        }
+
+        GhostAssembly findGhostAssembly(string assemblyName)
+        {
+            GhostAssembly ghostAssembly;
+            if(ghostAssemblies.TryGetValue(assemblyName, out ghostAssembly))
+            {
+                return ghostAssembly;
+            }
+
+            return null;
+        }
+
+        bool isGhost(System.Reflection.Assembly assembly)
+        {
+            foreach(var ghostAssembly in ghostAssemblies.Values)
+            {
+                if(ghostAssembly.Assembly == null)
+                {
+                    continue;
+                }
+
+                if(assembly == ghostAssembly.Assembly)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public string FindGhostAssemblyFile(string assemblyName)
+        {
+            var ghostAssembly = findGhostAssembly(assemblyName);
+            return ghostAssembly?.AssemblyFile;
+        }
+
+        static IDictionary<string, GhostAssembly> createGhostAssemblies(string[] assemblyFiles)
+        {
+            var ghostAssemblies = new Dictionary<string, GhostAssembly>();
+            foreach (var assemblyFile in assemblyFiles)
+            {
+                var assemblyName = Path.GetFileNameWithoutExtension(assemblyFile);
+                ghostAssemblies[assemblyName] = new GhostAssembly(assemblyFile);
+            }
+
+            return ghostAssemblies;
+        }
+
+        static string findAssemblyFile(string dir, string name)
+        {
+            var assemblyFile = Path.Combine(dir, name + ".dll");
+            if(File.Exists(assemblyFile))
+            {
+                return assemblyFile;
+            }
+
+            assemblyFile = Path.ChangeExtension(assemblyFile, "exe");
+            if (File.Exists(assemblyFile))
+            {
+                return assemblyFile;
+            }
+
+            return null;
+        }
+
+        class GhostAssembly
+        {
+            string assemblyFile;
+            System.Reflection.Assembly assembly;
+            DateTime lastWriteTime;
+
+            internal GhostAssembly(string assemblyFile)
+            {
+                this.assemblyFile = assemblyFile;
+            }
+
+            public string AssemblyFile => assemblyFile;
+
+            public System.Reflection.Assembly Assembly => assembly;
+
+            public System.Reflection.Assembly GetAssembly()
+            {
+                if(!File.Exists(assemblyFile))
+                {
+                    var message = GhostAssemblyException.CreateGhostNotFoundMessage(assemblyFile);
+                    throw new GhostAssemblyException(message);
+                }
+
+                var lastWriteTime = File.GetLastWriteTime(assemblyFile);
+                if (lastWriteTime != this.lastWriteTime)
+                {
+                    assembly = loadAssemblyFromBytes(assemblyFile);
+                    this.lastWriteTime = lastWriteTime;
+                }
+
+                return assembly;
+            }
+
+            static System.Reflection.Assembly loadAssemblyFromBytes(string assemblyFile)
+            {
+                var asmBytes = File.ReadAllBytes(assemblyFile);
+                var pdbFile = Path.ChangeExtension(assemblyFile, "pdb");
+                if (File.Exists(pdbFile))
+                {
+                    var pdbBytes = File.ReadAllBytes(pdbFile);
+                    return System.Reflection.Assembly.Load(asmBytes, pdbBytes);
+                }
+
+                return System.Reflection.Assembly.Load(asmBytes);
+            }
+        }
+
+#if DEBUG
+        class File
+        {
+            internal static Func<string, bool> Exists = (string path) => System.IO.File.Exists(path);
+            internal static Func<string, DateTime> GetLastWriteTime = (string path) => System.IO.File.GetLastWriteTime(path);
+            internal static Func<string, byte[]> ReadAllBytes = (string path) => System.IO.File.ReadAllBytes(path);
+        }
+
+        class Assembly
+        {
+            internal static Func<string, System.Reflection.Assembly> LoadFrom = (string assemblyFile) => System.Reflection.Assembly.LoadFrom(assemblyFile);
+        }
+#endif
+    }
+
+    public class GhostAssemblyException : Exception
+    {
+        public GhostAssemblyException(string message) : base(message)
+        {
+        }
+
+        public static string CreateGhostNotFoundMessage(string assemblyFile) =>
+            string.Format("Couldn't find ghost assembly at '{0}'.", assemblyFile);
+
+        public const string NoDefaultGhostMessage = "No default ghost assembly name name was specified.";
+    }
+}
---
+++ test/GhostAssemblies.Tests/GhostAssemblyLoaderTests.cs
@@ -0,0 +1,221 @@
+﻿namespace GhostAssemblies.Tests
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using StaticMocks;
+    using NSubstitute;
+    using NUnit.Framework;
+
+    public class GhostAssemblyLoaderTests
+    {
+        StaticMock staticMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            staticMock = new StaticMock(typeof(GhostAssemblyLoader));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            staticMock.Dispose();
+        }
+
+        [Test]
+        public void ResolveAssembly_NoDefaultGhostAssemblyName_ThrowsException()
+        {
+            using (var ghostAssemblyLoader = new GhostAssemblyLoader(null))
+            {
+                var expectMessage = GhostAssemblyException.NoDefaultGhostMessage;
+
+                var exception = Assert.Throws<GhostAssemblyException>(() => ghostAssemblyLoader.ResolveAssembly());
+
+                Assert.That(exception.Message, Is.EqualTo(expectMessage));
+            }
+        }
+
+        [Test]
+        public void ResolveAssembly_Default_IsSpecifiedDefaultGhostAssemblyName()
+        {
+            var testAssembly = GetType().Assembly;
+            var defaultName = testAssembly.GetName().Name;
+            var ghostAssemblyPaths = getFile(testAssembly);
+            using (var ghostAssemblyLoader = new GhostAssemblyLoader(ghostAssemblyPaths, defaultName))
+            {
+                var expectAssembly = ghostAssemblyLoader.ResolveAssembly(defaultName);
+
+                var defaultAssembly = ghostAssemblyLoader.ResolveAssembly();
+
+                Assert.That(defaultAssembly, Is.EqualTo(expectAssembly));
+            }
+        }
+
+        [Test]
+        public void ResolveAssembly_Unknown_ReturnsNull()
+        {
+            var unknownName = "UnknownAssemblyName";
+            var testAssembly = GetType().Assembly;
+            var defaultName = testAssembly.GetName().Name;
+            var ghostAssemblyPaths = getFile(testAssembly);
+            using (var ghostAssemblyLoader = new GhostAssemblyLoader(ghostAssemblyPaths, defaultName))
+            {
+                var unknownAssembly = ghostAssemblyLoader.ResolveAssembly(unknownName);
+
+                Assert.That(unknownAssembly, Is.Null);
+            }
+        }
+
+        [Test]
+        public void ResolveAssembly_DoesNotExist_ThrowsException()
+        {
+            var assemblyName = "DoesNotExist";
+            var assemblyFile = string.Format(@"\{0}.dll", assemblyName);
+            using (var ghostAssemblyLoader = new GhostAssemblyLoader(assemblyFile, assemblyName))
+            {
+                var expectMessage = GhostAssemblyException.CreateGhostNotFoundMessage(assemblyFile);
+
+                var exception = Assert.Throws<GhostAssemblyException>(() => ghostAssemblyLoader.ResolveAssembly());
+
+                Assert.That(exception.Message, Is.EqualTo(expectMessage));
+            }
+        }
+
+        [Test]
+        public void ResolveAssembly_NoChange_IsCached()
+        {
+            var testAssembly = GetType().Assembly;
+            var defaultName = testAssembly.GetName().Name;
+            var ghostAssemblyPaths = getFile(testAssembly);
+            using (var ghostAssemblyLoader = new GhostAssemblyLoader(ghostAssemblyPaths, defaultName))
+            {
+                var asm1 = ghostAssemblyLoader.ResolveAssembly();
+                var asm2 = ghostAssemblyLoader.ResolveAssembly();
+
+                Assert.That(asm2, Is.EqualTo(asm1));
+            }
+        }
+
+        [Test]
+        public void ResolveAssembly_NewTimeStamp_AssemblyChanged()
+        {
+            var testAssembly = GetType().Assembly;
+            var assemblyName = testAssembly.GetName().Name;
+            var assemblyFile = getFile(testAssembly);
+            using (var ghostAssemblyLoader = new GhostAssemblyLoader(assemblyFile, assemblyName))
+            {
+                staticMock.For(() => File.GetLastWriteTime(assemblyFile)).Returns(File.GetLastWriteTime(assemblyFile));
+                var asm1 = ghostAssemblyLoader.ResolveAssembly();
+                staticMock.For(() => File.GetLastWriteTime(assemblyFile)).Returns(DateTime.Now);
+                var asm2 = ghostAssemblyLoader.ResolveAssembly();
+
+                Assert.That(asm2, Is.Not.EqualTo(asm1));
+            }
+        }
+
+        [TestCase(@"\MockGhost.dll", "MockGhost", true, Description = "Load into 'Ghost' context")]
+        [TestCase(null, "MockLoadFrom", false, Description = "Load into 'LoadFrom' context")]
+        public void ResolveAssembly(string ghostAssemblyFile, string assemblyName, bool isGhost)
+        {
+            var ghostAssembly = typeof(Uri).Assembly;
+            var ghostAssemblyBytes = File.ReadAllBytes(ghostAssembly.Location);
+            staticMock.For(() => File.Exists(ghostAssemblyFile)).Returns(true);
+            staticMock.For(() => File.ReadAllBytes(ghostAssemblyFile)).Returns(ghostAssemblyBytes);
+
+            var loadFromAssembly = typeof(object).Assembly;
+            var appPath = getDirectory(Assembly.GetExecutingAssembly());
+            var appMockPath = Path.Combine(appPath, assemblyName + ".dll");
+            staticMock.For(() => File.Exists(appMockPath)).Returns(true);
+            staticMock.For(() => Assembly.LoadFrom(appMockPath)).Returns(loadFromAssembly);
+
+            var expectAssembly = isGhost ? ghostAssembly : loadFromAssembly;
+            using (var ghostAssemblyLoader = new GhostAssemblyLoader(ghostAssemblyFile))
+            {
+                var resolvedAssembly = ghostAssemblyLoader.ResolveAssembly(assemblyName);
+
+                Assert.That(resolvedAssembly.FullName, Is.EqualTo(expectAssembly.FullName));
+            }
+        }
+
+        [TestCase(@"\MockGhost.dll", "MockGhost", true, Description = "Load into 'Ghost' context")]
+        [TestCase(null, "MockLoadFrom", false, Description = "Load into 'LoadFrom' context")]
+        public void AssemblyResolve_CalledFromGhostAssembly(string ghostAssemblyPath, string assemblyName, bool isGhost)
+        {
+            var ghostAssembly = typeof(Uri).Assembly;
+            var ghostAssemblyBytes = File.ReadAllBytes(ghostAssembly.Location);
+            staticMock.For(() => File.Exists(ghostAssemblyPath)).Returns(true);
+            staticMock.For(() => File.ReadAllBytes(ghostAssemblyPath)).Returns(ghostAssemblyBytes);
+
+            var loadFromAssembly = typeof(object).Assembly;
+            var appPath = getDirectory(Assembly.GetExecutingAssembly());
+            var appMockPath = Path.Combine(appPath, assemblyName + ".dll");
+            staticMock.For(() => File.Exists(appMockPath)).Returns(true);
+            staticMock.For(() => Assembly.LoadFrom(appMockPath)).Returns(loadFromAssembly);
+
+            var expectAssembly = isGhost ? ghostAssembly : loadFromAssembly;
+
+            var resolvedAssembly = invokeAssemblyLoadFromGhostAssembly(ghostAssemblyPath, assemblyName);
+            Assert.That(resolvedAssembly.FullName, Is.EqualTo(expectAssembly.FullName));
+        }
+
+        Assembly invokeAssemblyLoadFromGhostAssembly(string ghostAssemblyPath, string assemblyName)
+        {
+            var execAsm = Assembly.GetExecutingAssembly();
+            var execAsmName = execAsm.GetName().Name;
+            var execAsmLocation = getFile(execAsm);
+            var execAssemblyBytes = File.ReadAllBytes(execAsmLocation);
+            staticMock.For(() => File.Exists(execAsmLocation)).Returns(true);
+            staticMock.For(() => File.ReadAllBytes(execAsmLocation)).Returns(execAssemblyBytes);
+
+            using (var ghostAssemblyLoader = new GhostAssemblyLoader(ghostAssemblyPath + ";" + execAsmLocation))
+            {
+                var asm = ghostAssemblyLoader.ResolveAssembly(execAsmName);
+                var ghostType = asm.GetType(GetType().FullName);
+                var method = ghostType.GetMethod(nameof(AssemblyLoad));
+
+                return (Assembly)method.Invoke(null, new object[] { assemblyName });
+            }
+        }
+
+        public static Assembly AssemblyLoad(string assemblyFullName)
+        {
+            return Assembly.Load(assemblyFullName);
+        }
+
+        [TestCase("Test", @"\BasePath\Test.dll", @"\BasePath\Test.dll", @"\BasePath\Test.dll")]
+        [TestCase("Test", @"\BasePath\Test.exe", @"\BasePath\Test.exe", @"\BasePath\Test.exe")]
+        [TestCase("Test", null, @"$(AppPath)\Test.exe", null)]
+        [TestCase("Test", @"\First\Test.exe", @"\First\Test.exe;Second\Test.exe", @"\First\Test.exe")]
+        public void FindGhostAssemblyFile(string assemblyName, string ghostAssemblyPaths, string filesExist, string expect)
+        {
+            var appPath = getDirectory(typeof(GhostAssemblyLoader).Assembly);
+            ghostAssemblyPaths = ghostAssemblyPaths?.Replace("$(AppPath)", appPath);
+            filesExist = filesExist.Replace("$(AppPath)", appPath);
+            expect = expect?.Replace("$(AppPath)", appPath);
+            foreach (var path in filesExist.Split(';'))
+            {
+                staticMock.For(() => File.Exists(path)).Returns(true);
+            }
+
+            using (var ghostAssemblyLoader = new GhostAssemblyLoader(ghostAssemblyPaths, assemblyName))
+            {
+                var assemblyFile = ghostAssemblyLoader.FindGhostAssemblyFile(assemblyName);
+
+                Assert.That(assemblyFile, Is.EqualTo(expect));
+            }
+        }
+
+        static string getFile(Assembly assembly)
+        {
+            return new Uri(assembly.EscapedCodeBase).LocalPath;
+        }
+
+        static string getDirectory(Assembly assembly)
+        {
+            var localPath = new Uri(assembly.EscapedCodeBase).LocalPath;
+            var directory = Path.GetDirectoryName(localPath);
+            return directory;
+        }
+    }
+}
