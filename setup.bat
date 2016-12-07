032e8c81c0c62bef492d590d3efd0d857a644f26
Jamie Cansdale <jcansdale@gmail.com>
2016-12-07 20:09:14 +0000

    Build GhostAssemblies NuGet package Download NuGet.CommandLine using Paket. Fixed .packet->.paket misspelling.

--- setup.bat
+++ setup.bat
@@ -1,5 +1,10 @@
 @call vars.bat
+
 paket.bootstrapper
 paket install
+
 set path=%cd%\packages\proj2src\lib\net452;%path%
 proj2src paket-files\jbevain\cecil\Mono.Cecil.csproj
+
+set path=%cd%\packages\NuGet.CommandLine\tools;%path%
+nuget pack src\GhostAssemblies\GhostAssemblies.csproj
