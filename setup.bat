295ea0d30e2cd2a4bd2e4049255b93cb60eb86d1
Jamie Cansdale <jcansdale@gmail.com>
2016-12-08 23:41:08 +0000

    Build before NuGet packaging

--- setup.bat
+++ setup.bat
@@ -6,5 +6,7 @@ paket install
 set path=%cd%\packages\proj2src\lib\net452;%path%
 proj2src paket-files\jbevain\cecil\Mono.Cecil.csproj
 
+msbuild GhostAssemblies.sln
+
 set path=%cd%\packages\NuGet.CommandLine\tools;%path%
 nuget pack src\GhostAssemblies\GhostAssemblies.csproj
